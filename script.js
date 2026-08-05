be18e4cdfa69c97f8894071673c6b3b76e7a97c3
limasouza17-droid <lima.souza17@escola.pr.gov.br>
2026-08-05 08:57:45 -0300

    js

--- script.js
+++ script.js
@@ -61,5 +61,16 @@ botaoGerar.addEventListener("click", () => {
 
 
     campoSenha.value = senha;
+    classificaSenha();
 
 });
+function classificaSenha() {
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (tamanhoSenha > 11) {
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5) {
+        forcaSenha.classList.add('fraca');
+    }
+}
